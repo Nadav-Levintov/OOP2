a6e8d9845399340c94884f7856c72e39b0b5959a
Nadav-Levintov <DSP155@Gmail.com>
2016-04-12 15:05:56 +0300

    Status

--- src/OOP2/Solution/StatusImpl.java
+++ src/OOP2/Solution/StatusImpl.java
@@ -1,13 +1,65 @@
 package OOP2.Solution;
 
+import OOP2.Provided.Person;
 import OOP2.Provided.Status;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class StatusImpl implements Status {
+
+	Person publisher;
+	String content;
+	Integer id;
+	Set<Person> likes;
+
 
 	/*
 	 * A constructor that receives the status publisher, the text of the status
 	 *  and the id of the status.
 	 */
 	public StatusImpl(Person publisher, String content, Integer id)
-	{}
+	{
+		this.id = id;
+		this.content = content;
+		this.publisher = publisher;
+		likes = new HashSet<Person>();
+
+	}
+
+	@Override
+	public Integer getId() {
+		return id;
+	}
+
+	@Override
+	public Person getPublisher() {
+		return publisher;
+	}
+
+	@Override
+	public String getContent() {
+		return content;
+	}
+
+	@Override
+	public void like(Person p) {
+		if(!likes.contains(p))
+		{
+			likes.add(p);
+		}
+	}
+
+	@Override
+	public void unlike(Person p) {
+		if(likes.contains(p))
+		{
+			likes.remove(p);
+		}
+	}
+
+	@Override
+	public Integer getLikesCount() {
+		return likes.size();
+	}
 }
