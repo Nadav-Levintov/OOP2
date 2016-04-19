f6fae8291f5c6bbdd4ab8b72570147b148755494
Nadav-Levintov <DSP155@Gmail.com>
2016-04-19 18:12:07 +0300

    FaceOOP

--- src/OOP2/Solution/FaceOOPImpl.java
+++ src/OOP2/Solution/FaceOOPImpl.java
@@ -1,13 +1,163 @@
 package OOP2.Solution;
 
-import OOP2.Provided.FaceOOP;
+import OOP2.Provided.*;
+
+import java.util.*;
 
 public class FaceOOPImpl implements FaceOOP {
+	HashMap<Integer,Person> personMap;
 
 	/**
 	 * Constructor - receives no parameters and initializes the system.
 	 */
 	public FaceOOPImpl()
-	{}
-	
+	{
+		personMap = new HashMap<Integer, Person>();
+
+	}
+
+	@Override
+	public Person joinFaceOOP(Integer id, String name) throws PersonAlreadyInSystemException {
+		Person p = new PersonImpl(id,name);
+		if(personMap.containsValue(p))
+		{
+			throw new PersonAlreadyInSystemException();
+		}
+		personMap.put(id,p);
+
+		return p;
+	}
+
+	@Override
+	public int size() {
+		return personMap.size();
+	}
+
+	@Override
+	public Person getUser(Integer id) throws PersonNotInSystemException {
+	if(personMap.containsKey(id))
+	{
+		throw new PersonNotInSystemException();
+	}
+		Person res = personMap.get(id);
+
+		return res;
+	}
+
+    @Override
+    public void addFriendship(Person p1, Person p2) throws PersonNotInSystemException, SamePersonException, ConnectionAlreadyExistException {
+
+        if(!personMap.containsValue(p1) ||  !personMap.containsValue(p2))
+        {
+            throw new PersonNotInSystemException();
+        }
+        if(p1.equals(p2))
+        {
+            throw new SamePersonException();
+        }
+
+        p1.addFriend(p2);
+        p2.addFriend(p1);
+
+    }
+
+    @Override
+    public StatusIterator getFeedByRecent(Person p) throws PersonNotInSystemException {
+       if(!personMap.containsValue(p))
+       {
+           throw new PersonNotInSystemException();
+       }
+        Collection<Person> friends = p.getFriends();
+
+        List<Person> friendsList = new ArrayList<Person>(friends);
+
+        Collections.sort(friendsList);
+
+        LinkedList<Status> feed = new LinkedList<Status>();
+
+        for (Person curr : friendsList)
+        {
+            Iterable<Status> temp = curr.getStatusesRecent();
+            for (Status s : temp)
+            {
+                feed.addLast(s);
+            }
+
+        }
+
+        StatusIterator res = (StatusIterator)feed.iterator();
+        return res;
+    }
+
+    @Override
+    public StatusIterator getFeedByPopular(Person p) throws PersonNotInSystemException {
+        if(!personMap.containsValue(p))
+        {
+            throw new PersonNotInSystemException();
+        }
+        Collection<Person> friends = p.getFriends();
+
+        List<Person> friendsList = new ArrayList<Person>(friends);
+
+        Collections.sort(friendsList);
+
+        LinkedList<Status> feed = new LinkedList<Status>();
+
+        for (Person curr : friendsList)
+        {
+            Iterable<Status> temp = curr.getStatusesPopular();
+            for (Status s : temp)
+            {
+                feed.addLast(s);
+            }
+
+        }
+
+        StatusIterator res = (StatusIterator)feed.iterator();
+        return res;
+    }
+
+    @Override
+    public Integer rank(Person source, Person target) throws PersonNotInSystemException, ConnectionDoesNotExistException {
+        if(!personMap.containsValue(source)||!personMap.containsValue(target))
+        {
+            throw new PersonNotInSystemException();
+        }
+        if(source.equals(target))
+        {
+            return 0;
+        }
+
+        LinkedList<Person> q = new LinkedList<Person>();
+        HashMap<Person,Integer> graph = new HashMap<Person, Integer>();
+        for(Person p : personMap.values())
+        {
+            graph.put(p,Integer.MAX_VALUE);
+        }
+        graph.put(source,0);
+        q.addLast(source);
+        while(!q.isEmpty())
+        {
+            Person curr = q.removeFirst();
+            for(Person p: curr.getFriends())
+            {
+                if(graph.get(p).equals(Integer.MAX_VALUE))
+                {
+                    graph.put(p,graph.get(curr)+1);
+                    q.addLast(p);
+                }
+            }
+        }
+        if(graph.get(target).equals(Integer.MAX_VALUE))
+        {
+            throw new ConnectionDoesNotExistException();
+        }
+
+        return graph.get(target);
+    }
+
+    @Override
+    public Iterator<Person> iterator() {
+        return personMap.values().iterator();
+    }
 }
